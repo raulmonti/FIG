d32b47c5098eff0d96125d0624c28c4cb7771ea8
Carlos E. Budde <cbudde@famaf.unc.edu.ar>
2015-07-07 12:44:39 -0300

    Properly traversing edges, now trying to compute weighted shortest distance

---
+++ src/bgl_graph_structure_change.cpp
@@ -0,0 +1,21 @@
+#include <iostream>    // std::cout
+#include <string>      // std::string
+#include <vector>      // std:::vector<>
+#include <tuple>       // std::tuple<>
+#include <utility>     // std::pair
+#include <algorithm>   // std::for_each, std::swap
+#include <unistd.h>    // EXIT_{SUCCESS,FAILURE}
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/compressed_sparse_row_graph.hpp>  // TODO: make available
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
+
+int main (int, char**)
+{
+	// TODO
+	//     Create adjacency list graph and transform it into a CSR graph
+	//     Print both structures to check equality
+
+	return EXIT_SUCCESS;
+}
--- src/bgl_weighted_graph.cpp
+++ src/bgl_weighted_graph.cpp
@@ -1,7 +1,9 @@
 #include <iostream>    // std::cout
 #include <string>      // std::string
+#include <vector>      // std:::vector<>
+#include <tuple>       // std::tuple<>
 #include <utility>     // std::pair
-#include <algorithm>   // std::for_each
+#include <algorithm>   // std::for_each, std::swap
 #include <unistd.h>    // EXIT_{SUCCESS,FAILURE}
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
@@ -9,21 +11,40 @@
 
 
 
-typedef std::string str;
-typedef std::pair<str,float> Pair;
+namespace bgl = boost;
+
+using std::string;
+
+typedef std::pair<string,float> Pair;
+
+class GraphException : public std::exception
+{
+	string msg;
+	virtual const char* what() const throw() {
+		return string("Exception raised: ").append(msg).c_str();
+	}
+public:
+	GraphException(const char* _msg) : msg(_msg) {}
+};
 
 
 // ///////////////////////////////////////////////////////////////////////////
 //
 //	NOTE: will use Boost bundled properties (http://goo.gl/mJS8gP)
 //
-//	typedef boost::property<boost::vertex_distance_t, float,
-//			boost::property<boost::vertex_name_t, std::string>>
+//	typedef bgl::property<bgl::vertex_distance_t, float,
+//			bgl::property<bgl::vertex_name_t, std::string>>
 //		VertexProperty;
-//	typedef boost::property<boost::edge_weight_t, float>
+//	typedef bgl::property<bgl::edge_weight_t, float>
 //		EdgeProperty;
-struct Importance {
-	Importance(const str& name_, const float& importance_) :
+//	typedef bgl::adjacency_list<
+//		bgl::listS, bgl::vecS, bgl::directedS,
+//		VertexProperty, EdgeProperty> Graph;
+//
+struct Importance
+{
+	Importance() : name(""), importance(0) {}
+	Importance(const string& name_, const float& importance_) :
 		name(name_),
 		importance(importance_)
 	{}
@@ -31,61 +52,66 @@ struct Importance {
 		name(imp.first),
 		importance(imp.second)
 	{}
-	str   name;
-	float importance;
+	string name;
+	float  importance;
 };
-struct Probability {
+struct Probability
+{
+	Probability() : weight(0.0) {}
+	Probability(float w) : weight(w) {
+		if (0.0 > w || w > 1.0)
+			throw GraphException("probability values ∈ [0,1]");
+	}
+	Probability& operator=(Probability that) {
+		std::swap(weight, that.weight);
+	}
 	float weight;
 };
-//	typedef boost::adjacency_list<
-//		boost::listS, boost::vecS, boost::directedS,
-//		VertexProperty, EdgeProperty> Graph;
-typedef boost::adjacency_list<
-	boost::listS, boost::vecS, boost::directedS,
-	Importance, Probability> Graph;
+typedef bgl::adjacency_list<
+	boost::listS, bgl::vecS, bgl::directedS,
+	Importance,   Probability>
+Graph;
 //
 // ///////////////////////////////////////////////////////////////////////////
 
 
+
 // Descritpors
-typedef boost::graph_traits<Graph>::vertex_descriptor  VerDes;
-typedef boost::graph_traits<Graph>::edge_descriptor    EdgDes;
+typedef bgl::graph_traits<Graph>::vertex_descriptor  VerDes;
+typedef bgl::graph_traits<Graph>::edge_descriptor   EdgeDes;
 // Iterators
-typedef boost::graph_traits<Graph>::vertex_iterator  VerIter;
-typedef boost::graph_traits<Graph>::edge_iterator    EdgIter;
+typedef bgl::graph_traits<Graph>::vertex_iterator  VerIter;
+typedef bgl::graph_traits<Graph>::edge_iterator   EdgeIter;
 // Vertices maps
-typedef boost::property_map<Graph,boost::vertex_index_t>::type   VertIndexMap;
-typedef boost::property_map<Graph,boost::vertex_bundle_t>::type  VertPropMap;
-// Edges maps
-//typedef boost::property_map<Graph,boost::edge_index_t>::type   EdgeIndexMap;
-//typedef boost::property_map<Graph,boost::edge_bundle_t>::type  EdgePropMap;
+typedef bgl::property_map<Graph,bgl::vertex_index_t>::type   VertIndexMap;
+typedef bgl::property_map<Graph,bgl::vertex_bundle_t>::type  VertPropMap;
+
 
 
 int main(int, char**)
 {
-
 	// Graph object, so far unpopulated
 	Graph G;
 
 	// Vertices properties
 	Importance nodesImportance[] = {
-		Pair(   "0", 0.0),
-		Pair(   "1", 0.0),
-		Pair(   "2", 0.0),
-		Pair(   "3", 0.0),
+		Pair("Juan", 0.0),
+		Pair("Pepe", 0.0),
+		Pair(  "Ro", 0.0),
+		Pair( "Ita", 0.0),
 		Pair("RARE", 1.0)
 	};
 	const int numNodes(sizeof(nodesImportance)/sizeof(nodesImportance[0]));
 	// Populate the graph with them
 	for (int i=0 ; i<numNodes ; i++)
-		boost::add_vertex(nodesImportance[i], G);
+		bgl::add_vertex(nodesImportance[i], G);
 	// Show them
-	std::cout << "Num vertices: " << boost::num_vertices(G) << std::endl;
+	std::cout << "Num vertices: " << bgl::num_vertices(G) << std::endl;
 	std::cout << "Vertices:" << std::endl;
-	VertIndexMap viMap = boost::get(boost::vertex_index,  G);
-	VertPropMap  vpMap = boost::get(boost::vertex_bundle, G);
+	VertIndexMap viMap = bgl::get(bgl::vertex_index,  G);
+	VertPropMap  vpMap = bgl::get(bgl::vertex_bundle, G);
 	VerIter vit, vit_end;
-	for (boost::tie(vit,vit_end) = boost::vertices(G); vit != vit_end ; vit++) {
+	for (bgl::tie(vit,vit_end) = bgl::vertices(G); vit != vit_end ; vit++) {
 		std::cout << "  "
 				  << "[" << viMap[*vit] << "]: "
 				  << "(" << vpMap[*vit].name
@@ -94,32 +120,58 @@ int main(int, char**)
 	}
 	std::cout << std::endl;
 
-//	// Edges properties
-//	typedef std::pair<int,int> Edge;
-//	const Edge edges[numEdges] = {
-//		Edge(vmap[0],vmap[2]),
-//		Edge(vmap[1],vmap[1]), Edge(vmap[1],vmap[3]), Edge(vmap[1],vmap[4]),
-//		Edge(vmap[2],vmap[1]), Edge(vmap[2],vmap[3]),
-//		Edge(vmap[3],vmap[4]),
-//		Edge(vmap[4],vmap[0]), Edge(vmap[4],vmap[1])
-//	};
-//	const int numEdges(sizeof(edges)/sizeof(edges[0]));
-//	const Probability edgesWeight[numEdges] = {
-//		1.0,
-//		0.3, 0.5, 0.2,
-//		0.7, 0.3,
-//		1.0,
-//		0.5, 0.5
-//	};
-//	// Graph
-//	for (int j=0 ; j<numEdges ; j++)
-//		boost::add_edge()
-//	Graph G(edges,
-//			edges + sizeof(edges) / sizeof(Edge),
-//			nodesImportance,
-//			edgesWeight,
-//			boost::graph_property<Graph,>::type,
-//			numNodes);
+	// Edges properties
+	typedef std::tuple<int,int,Probability> Edge;
+	const std::vector<Edge> edges = {
+		std::make_tuple(viMap[0], viMap[2], 1.0),
+		std::make_tuple(viMap[1], viMap[1], 0.3),
+		std::make_tuple(viMap[1], viMap[3], 0.5),
+		std::make_tuple(viMap[1], viMap[4], 0.2),
+		std::make_tuple(viMap[2], viMap[1], 0.7),
+		std::make_tuple(viMap[2], viMap[3], 0.3),
+		std::make_tuple(viMap[3], viMap[4], 1.0),
+		std::make_tuple(viMap[4], viMap[0], 0.5),
+		std::make_tuple(viMap[4], viMap[1], 0.5)
+	};
+	// Populate graph with them
+	for (auto it = edges.begin() ; it != edges.end() ; it++) {
+		int s, t;
+		Probability p;
+		std::tie(s, t, p) = *it;
+		bgl::add_edge(s, t, p, G);
+	}
+	// Show them
+	std::cout << "Num edges:" << bgl::num_edges(G) << std::endl;
+	std::cout << "Edges:" << std::endl;
+	EdgeIter eit, eit_end;
+	for (bgl::tie(eit, eit_end) = bgl::edges(G) ; eit != eit_end ; eit++) {
+		// For bgl::adjacency_list there is no indexed access of edges
+		// (see http://goo.gl/Xdq9If) We use iterators for that.
+		std::cout << " "
+				  << "(" << bgl::source(*eit,G)
+				  << "," << bgl::target(*eit,G) << "): "
+				  << G[*eit].weight   // BLACK MAGIC: http://goo.gl/VC16UB
+				  << std::endl;
+	}
+	std::cout << std::endl;
+
+	// Find distance of each vertex to "RARE"
+	vit = bgl::vertices(G).first;
+	std::advance(vit, bgl::num_vertices(G)-1);
+	VerDes rare = *vit;
+	std::vector<int> distance(bgl::num_vertices(G));
+
+//	TODO study "Extending algorithms with visitors" from the BGL tuto
+
+	//	bgl::dijkstra_shortest_paths(G, rare, bgl::distance_map(&distance[0]));
+	std::cout << "Distance to rare vertex:" << std::endl;
+	viMap = bgl::get(bgl::vertex_index, G);
+	for (bgl::tie(vit, vit_end) = bgl::vertices(G) ; vit != vit_end ; vit++) {
+		std::cout << " "
+				  << viMap[*vit]    << " --> "
+				  << G[rare].name   << " : ";
+//				  << distance[*vit] << std::endl;
+	}
 
 	return EXIT_SUCCESS;
 }
