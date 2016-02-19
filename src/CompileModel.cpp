634efed17ea7970e613132a2df14e05a94e6f042
Raul <raulmonti@raulmonti.com>
2016-02-19 19:45:59 -0300

    Fixing module CompileMpdel

--- src/CompileModel.cpp
+++ src/CompileModel.cpp
@@ -106,6 +106,7 @@ CompileTransition(AST* trans)
         pre = ASTpre->toString();
         nl = ASTpre->get_all_lexemes(_NAME);
     }
+
     vector<AST*> assigs = trans->get_all_ast(_ASSIG);
     string lassig; // left side of assignment
     vector<string> rassig; // right side of assignment
@@ -113,15 +114,15 @@ CompileTransition(AST* trans)
     for(const auto &it: assigs){
         string name = it->get_lexeme(_NAME);
         name.pop_back();
+        cout << "##" << name << endl;
         AST* ASTassig = it->get_first(_EXPRESSION); 
-        if(ASTassig){        
-            string assig = ASTassig->toString();
-            lassig += assig;
-            lassig += ",";
-            rassig.push_back(assig);
-            vector<string> vars = ASTassig->get_all_lexemes(_NAME);
-            variables.insert(variables.end(), vars.begin(), vars.end());
-        }
+        assert(ASTassig);        
+        string assig = ASTassig->toString();
+        lassig += assig;
+        lassig += ",";
+        rassig.push_back(name);
+        vector<string> vars = ASTassig->get_all_lexemes(_NAME);
+        variables.insert(variables.end(), vars.begin(), vars.end());
     }
     if(lassig != ""){
         lassig.pop_back();    
@@ -133,6 +134,13 @@ CompileTransition(AST* trans)
         setcs.push_back(sc);
     }
 
+    cout << "#3 " << lassig << endl;
+    for(const auto &it: rassig){
+        cout << "#4 " << it << endl;
+    }
+    for(const auto &it: variables){
+        cout << "#5 " << it << endl;
+    }
     fig::Transition result( Label(action,io)
                           , eclk
                           , Precondition(pre, nl)
