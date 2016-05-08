352646c7f40ba883b1df1bc5389541a9246b3c35
Carlos E. Budde <cbudde@famaf.unc.edu.ar>
2016-05-07 21:01:50 -0300

    3-queue tandem network experiment

--- include/FigConfig.h
+++ include/FigConfig.h
@@ -2,4 +2,4 @@
 #define fig_VERSION_MAJOR 1
 #define fig_VERSION_MINOR 0
 // FIG tool current build
-#define fig_CURRENT_BUILD "19:56:15 06.05.2016 (release)"
+#define fig_CURRENT_BUILD "19:47:21 07.05.2016 (release)"
