raulmonti/FIG

commit 1c9c63fe766ebf1dc4e47382c64c0eb499ec30d2

Looks like we fixed a BUG. I sugest Charlos to give his opinion about it (module ImportanceFunctionConcreteSplit method advance).

src/ImportanceFunctionConcreteSplit.cpp
@@ -85,10 +85,11 @@ bool advance(const ExtremeValuesVec& moduleValues, ImportanceVec& values)
 	// Increment it
 	values[pos]++;
 	// Reset previous values (according to 'moduleValues' order) to minimums
-	unsigned i(0u);
+    unsigned i(0u);
 	for (const auto& e: moduleValues)
-		if (i < pos)
-			values[i++] = std::get<0>(e);
+        if (i < pos)
+            values[i] = std::get<0>(e);
+            i++;
 	return true;
 }
 
@@ -127,11 +128,10 @@ find_extreme_values(const Formula& f, const ExtremeValuesVec& moduleValues)
 
 	// Test all values combinations for the relevant variables
 	do {
-		ImportanceValue imp = f(values);
-		min = std::min(min, imp);
-		max = std::max(max, imp);
-	} while (advance(moduleValues, values));
-
+        ImportanceValue imp = f(values);
+        min = std::min(min, imp);
+        max = std::max(max, imp);
+    } while (advance(moduleValues, values));
 	// Play it safe and assume minRareValue_ == minValue_
 	return std::make_tuple(min, max, min);
 
@@ -216,7 +216,7 @@ find_extreme_values(const Formula& f,
 		break;
 
 	case CompositionType::AD_HOC:
-		return find_extreme_values(f, moduleValues);
+        return find_extreme_values(f, moduleValues);
 
 	default:
 		throw_FigException("unrecognized composition function strategy: "
@@ -524,7 +524,6 @@ ImportanceFunctionConcreteSplit::assess_importance(const Property& prop,
 	// If the rare event depends on the state of more than one module,
 	// global rarity can't be encoded split in vectors for later simulations
 	concreteSimulation_ = numRelevantModules < 2u;
-
 	// Apply post processing (shift, exponentiation, etc.)
 	if ("flat" != strategy) {
 		post_process(postProc, moduleValues);
@@ -544,16 +543,15 @@ ImportanceFunctionConcreteSplit::assess_importance(const Property& prop,
 		minValue_ = userMinValue_;
 		maxValue_ = userMaxValue_;
 		minRareValue_ = userMinValue_;  // play it safe
-
 	} else if (globalStateCopy.concrete_size() > uint128::uint128_0 &&
 			   globalStateCopy.concrete_size() < (1ul<<20ul)) {
 		// A brute force, full-state-space scan is affordable
 		find_extreme_values(globalStateCopy, prop);
-
 	} else {
 		// Concrete state space is too big, resort to smarter ways
 		std::tie(minValue_, maxValue_, minRareValue_) =
 				::find_extreme_values(userFun_, moduleValues, compositionStrategy_);
+
 	}
 	initialValue_ = importance_of(systemInitialValuation);
 

models/db_with_a_repairbox_generator.py
@@ -249,7 +249,7 @@ def disk_things_generator(string_template):
 # 		prop += "true | " + os.linesep 
 # 	return prop
 
-def cont_model_generator(string_template, last_part):
+def cont_model_generator(string_template, last_part, white_space):
 	controllers = {}
 	for module in control_modules:
 		controllers[module] = []
@@ -260,7 +260,7 @@ def cont_model_generator(string_template, last_part):
 	prop = ""
 	data = {}
 	for module in control_modules:
-		prop += "    "
+		prop += white_space
 		for i in range(0, len(controllers[module])):
 			(data['M1'],data['N1']) = controllers[module][i]
 			prop += template.substitute(data)
@@ -269,7 +269,7 @@ def cont_model_generator(string_template, last_part):
 
 
 
-def proc_model_generator(string_template, last_part):
+def proc_model_generator(string_template, last_part, white_space):
 	processors = {}
 	for module in process_modules:
 		processors[module] = []
@@ -280,7 +280,7 @@ def proc_model_generator(string_template, last_part):
 	prop = ""
 	data = {}
 	for module in process_modules:
-		prop += "    "
+		prop += white_space
 		for i in range(0, len(processors[module])):
 			(data['M1'],data['N1']) = processors[module][i]
 			prop += template.substitute(data)
@@ -290,15 +290,15 @@ def proc_model_generator(string_template, last_part):
 print """properties
     S ( """
 print disk_things_generator("    (dstatus_${M1}_${N1} > 0 & dstatus_${M2}_${N2} > 0) |" + os.linesep)
-print cont_model_generator("(cstatus_${M1}_${N1} > 0) & ", "true | " + os.linesep )
-print proc_model_generator("(pstatus_${M1}_${N1} > 0) & ", "true | " + os.linesep)
+print cont_model_generator("(cstatus_${M1}_${N1} > 0) & ", "true | " + os.linesep, "    ")
+print proc_model_generator("(pstatus_${M1}_${N1} > 0) & ", "true | " + os.linesep, "    ")
 print """    false ) 
 endproperties"""
 
 
 
-pp =  disk_things_generator("(Disk_${M1}_${N1} * Disk_${M2}_${N2}) + ")
-pp += cont_model_generator("Controller_${M1}_${N1} * ", " 1  + ")
-pp += cont_model_generator("Processor_${M1}_${N1} * ", " 1  + ") 
+pp =  disk_things_generator("(Disk_${M1}_${N1}*Disk_${M2}_${N2})+")
+pp += cont_model_generator("Controller_${M1}_${N1}*", "1+", "")
+pp += cont_model_generator("Processor_${M1}_${N1}*", "1+", "") 
 pp += "0"
 print "//", pp

src/ModelSuite.cpp
@@ -829,7 +829,6 @@ ModelSuite::build_importance_function_auto(const ImpFunSpec& impFun,
         techLog_ << "Property: " << property.to_string() << std::endl;
 		ifun.clear();
 		const double startTime = omp_get_wtime();
-
 		// Compositional importance functions need a composition function
 		if (impFun.name.find("split") != std::string::npos)
 			static_cast<ImportanceFunctionConcreteSplit&>(ifun)
@@ -837,11 +836,11 @@ ModelSuite::build_importance_function_auto(const ImpFunSpec& impFun,
 									 impFun.neutralElement,
 									 impFun.minValue,
 									 impFun.maxValue);
+
 		try {
 			// Compute importance automatically -- here hides the magic!
-			static_cast<ImportanceFunctionConcrete&>(ifun)
+            static_cast<ImportanceFunctionConcrete&>(ifun)
 					.assess_importance(property, "auto", impFun.postProcessing);
-
 		} catch (std::bad_alloc&) {
 			throw_FigException("couldn't build importance function \""
 							   + impFun.name + "\" automatically: not enough "

commit 0b008a2082bdf4644afd98dea53629a1fba264ce

Two bugfixes.
Fixed a bug introduced in 1c9c63fe766ebf1dc4e47382c64c0eb499ec30d2
[ ImportanceFunctionConcreteSplit::advance() source file ]
Left comment in situ & in GitLab website.

Fixed a bug introduced in b481186573f0fbeb7845b142d6a6b00d54e86917
[ ModuleInstance & Traial classes ]
The changes in the layout of the passive jump in ModuleInstance
imply that *not* all clocks will be updated with positive values,
e.g. a clock may be "updated" with its former, negative value.
This made an assertion in Traial::update_clocks() fail;
since the assertion is no longer relevant, it has been deleted.

build_project.sh
@@ -118,8 +118,8 @@ if [ -z "$NJOBS" ]; then NJOBS=2; fi
 #OPTS="$OPTS -DBUILTIN_RNG=ON"  # Cmake build options, see CMakeLists.txt
 #OPTS="$OPTS -DPROFILING=ON"    # Cmake build options, see CMakeLists.txt
 mkdir -p $BUILD_DIR && cd $BUILD_DIR
-CC=$CC CXX=$CXX cmake $CMAKE_DIR $OPTS && make -j$NJOBS && \
-#CC=gcc CXX=g++ cmake $CMAKE_DIR $OPTS && make -j$NJOBS && \
+#CC=$CC CXX=$CXX cmake $CMAKE_DIR $OPTS && make -j$NJOBS && \
+CC=gcc CXX=g++ cmake $CMAKE_DIR $OPTS && make -j$NJOBS && \
 cd $CWD
 
 # Symlink main executable in current dir and in BUILD_BASE

include/Traial.h
@@ -281,7 +281,6 @@ public:  // Utils
 			auto clkValIter = begin(clockValues);
 			for (size_t i = firstClock ; i < firstClock+numClocks ; i++) {
 				assert(clkValIter != end(clockValues));
-				assert(0.0 < *clkValIter);
 				clocks_[i].value = *clkValIter;
 				clkValIter++;
 			}

src/ImportanceFunctionConcreteSplit.cpp
@@ -85,11 +85,12 @@ bool advance(const ExtremeValuesVec& moduleValues, ImportanceVec& values)
 	// Increment it
 	values[pos]++;
 	// Reset previous values (according to 'moduleValues' order) to minimums
-    unsigned i(0u);
+	unsigned i(0u);
 	for (const auto& e: moduleValues)
-        if (i < pos)
-            values[i] = std::get<0>(e);
-            i++;
+		if (i < pos)
+			values[i++] = std::get<0>(e);
+	        // XXX If you really want the "i++" out of the vector access,
+	        //	 *please* put braces for the scopes of the "for" and the "if"
 	return true;
 }
 

src/main.cpp
@@ -40,8 +40,8 @@
 #include <cassert>
 #include <sys/stat.h>
 // FIG
-#include <fig.h>
 #include <fig_cli.h>
+#include <FigConfig.h>
 #include <string_utils.h>
 #include <ModelBuilder.h>
 #include <ModelReductor.h>