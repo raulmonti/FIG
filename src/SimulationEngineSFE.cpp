be7d99d2b958f9bdfde7a1a3110a0d55661034ab
Carlos E. Budde <c.e.budde@utwente.nl>
2017-12-25 20:08:58 +0100

    Updates to SimulationEngineSFE::fixed_effort() -- UNFINISHED

--- src/SimulationEngineSFE.cpp
+++ src/SimulationEngineSFE.cpp
@@ -27,7 +27,7 @@
 //==============================================================================
 
 // C++
-#include <algorithm>  // std::fill(), std::move()
+#include <algorithm>  // std::fill(), std::max_element(), std::move()
 // FIG
 #include <SimulationEngineSFE.h>
 #include <PropertyTransient.h>
@@ -43,18 +43,61 @@ namespace   // // // // // // // // // // // // // // // // // // // // // // //
 {
 
 using fig::Traial;
-using TraialRef = fig::Reference< Traial >;
+using TraialVec = std::vector< fig::Reference< Traial > >;
+using fig::ImportanceValue;
 
-/// @brief From several potential "next step values" select the most likely one
-/// @details The vector given as argument contains several Traial instances
-///          that could be the next step of a Fixed Effort sweep.
-///          Using \p ifun check which ImportanceValue has the most occurrences,
-///          and delete from the vector any Traial that doesn't has a different
-///          ImportanceValue.
-void filter_next_value(std::vector< TraialRef >& traialsNext,
-                       const fig::ImportanceFunction& ifun)
+/**
+ * @brief Select the most likely "next step value" from several posibilities
+ *
+ *        The vector given as argument contains several Traial instances
+ *        that could be the next step of a Fixed Effort sweep.
+ *        Using \p ifun check which ImportanceValue has the most occurrences,
+ *        and delete from the vector any Traial with different ImportanceValue.
+ *
+ * @param traials    Traials to analyse and filter
+ * @param traialSink Container where discarded Traials will be placed
+ * @param reachCount Vector stating how often each ImportanceValue was reached,
+ *                   from among the importance values that occurr in \p traials
+ * @param ifun       Importance function to assess the importance of each Traial
+ *
+ * @return Next step value, viz. ImportanceValue of the chosen (surviving) Traials
+ */
+ImportanceValue
+filter_next_value(TraialVec& traials,
+                  TraialVec& traialSink,
+                  std::vector< size_t >& reachCount,
+                  const fig::ImportanceFunction& ifun)
 {
-	/// @todo TODO implement!
+    assert(!traials.empty());
+    decltype(traials) chosenTraials;
+    const auto N(traials.size());
+    chosenTraials.reserve(N);
+
+    auto nextStep = std::max_element(begin(reachCount), end(reachCount));
+    const ImportanceValue nextValue(*nextStep);
+    const size_t nextValueIndex(std::distance(begin(reachCount), nextStep));
+
+    /// @todo TODO Find the most frequent ImportanceValue from the reachCount
+    ///            passed as argument.
+    ///       That vector is the sizer of the Importance range, could be huge:
+    ///       Request another TAD? e.g. a map<ImportanceValue,unsigned>
+    ///                            where map[i] has the #(occurrences)
+    ///                            of ImportanceValue 'i' in traials
+
+
+
+    for (auto i = 0ul ; i < N ; i++) {
+        Traial& t(traials.back());
+        traials.pop_back();
+        if (ifun.importance_of(t.state) == nextValue)
+            chosenTraials.push_back(std::move(t));
+        else
+            traialSink.push_back(std::move(t));
+    }
+
+    /// @todo TODO finish implementation
+
+    return nextValue;
 }
 
 } // namespace   // // // // // // // // // // // // // // // // // // // // //
@@ -90,90 +133,120 @@ SimulationEngineSFE::fixed_effort(const ThresholdsVec& thresholds,
 	const size_t LVL_MAX(impFun_->max_value()+1),
 				 LVL_INI(impFun_->initial_value()),
 				 EFF_MAX(lvl_effort(impFun_->max_thresholds_effort()));
-
+    // Init result
+    if (result.empty())
+        result.insert(ThresholdsPathProb());
+    auto pathFound(*result.begin());
+    pathFound.reserve(thresholds.size());
+    pathFound.clear();
+    // Init internal TADs
 	std::vector< Reference< Traial > > traialsNow, traialsNext;
 	traialsNow.reserve(EFF_MAX);
 	traialsNext.reserve(EFF_MAX);
 	if (traials_.size() < EFF_MAX)
 		TraialPool::get_instance().get_traials(traials_, EFF_MAX);
-	if (reachCount_.size() != LVL_MAX)
-		decltype(reachCount_)(LVL_MAX,0).swap(reachCount_);
-	for (auto j = 0ul ; j < lvl_effort(impFun_->effort_of(LVL_INI)) ; j++) {
-		traials_.back().get().initialise(*model_,*impFun_);
-		traialsNext.push_back(std::move(traials_.back()));
-		traials_.pop_back();
-	}
+    if (reachCount_.size() != LVL_MAX)
+        decltype(reachCount_)(LVL_MAX,0).swap(reachCount_);
+    // Bootstrap Fixed Effort loop below
+    traials_.back().get().initialise(*model_, *impFun_);
+    traialsNow.push_back(std::move(traials_.back()));
+    traials_.pop_back();
 
 
-	size_t numSuccesses;
+    // Fixed Effort loop: For each threshold level 'l' ...
+    size_t numSuccesses;
 	ImportanceValue l(LVL_INI);
 	do {
-		numSuccesses = 0ul;
+        // ... prepare the Traials to run the simulations ...
+        numSuccesses = 0ul;
 		const auto LVL_EFFORT(lvl_effort(impFun_->effort_of(l)));
 		assert(0 < LVL_EFFORT);
-		assert(traialsNow.empty());
-		assert(!traialsNext.empty());
+        assert(traialsNow.empty());
+        assert(!traialsNext.empty());
+        for (auto i = 0ul ; i < LVL_EFFORT ; i++) {
+            const bool useFresh(i >= traialsNext.size());
+            Traial& traial(useFresh ? traials_.back() : traialsNext[i]);
+            if (useFresh) {
+                traials_.pop_back();
+                traial = traialsNext[i%traialsNext.size()].get();  // copy *contents*
+            }
+            assert(traial.level == l);
+            traial.depth = 0;
+            traialsNow.push_back(traial);
+        }
+        // ... run Fixed Effort until any level > 'l' ...
+        for (auto i = 0ul ; i < LVL_EFFORT ; i++) {
+            Traial& traial(traialsNow.back());
+            traialsNow.pop_back();
+            assert(traial.level < LVL_MAX);
+//			reachCount_[traial.level]++;
+            network.simulation_step(traial, property, event_watcher);
+//          network.simulation_step(traial, property, *this, &fig::SimulationEngineSFE::transient_event);
+            if (traial.level > l) {
+                numSuccesses++;
+                traialsNext.push_back(traial);
+                reachCount_[l]++;
+            } else {
+                if (property.is_rare(traial.state))
+                    numSuccesses++;
+                traials_.push_back(traial);
+            }
+        }
+        // ... and interpret the results
+        l = filter_next_value(traialsNext, traials_, reachCount_, *impFun_);
 
+        /// @todo TODO implement function above!
 
-
-
-		filter_next_value(traialsNext, *impFun_);
-
-
-
+        assert(l <= LVL_MAX);
+        pathFound.emplace_back(l, static_cast<double>(numSuccesses)/LVL_EFFORT);
 	} while (l < LVL_MAX && traialsNext.size() > 0ul);
 
 
-
-
-
-
-
-	// For each threshold level 'l' ...
-	ImportanceValue l;
-	size_t numSuccesses(1ul);
-	for (l = LVL_INI ; l < LVL_MAX && 0ul < traialsNext.size() ; l++) {
-		// ... prepare the Traials to run the simulations ...
-		numSuccesses = 0ul;
-		const auto LVL_EFFORT(lvl_effort(impFun_->effort_of(l)));
-		assert(0 < LVL_EFFORT);
-		assert(traialsNow.empty());
-		assert(!traialsNext.empty());
-		for (auto j = 0ul ; j < LVL_EFFORT ; j++) {
-			const bool useFresh(j >= traialsNext.size());
-			Traial& traial(useFresh ? traials_.back() : traialsNext[j]);
-			if (useFresh) {
-				traials_.pop_back();
-				traial = traialsNext[j%traialsNext.size()].get();  // copy *contents*
-			}
-			assert(traial.level == l);
-			traial.depth = 0;
-			traialsNow.push_back(traial);
-		}
-		assert(traialsNow.size() == LVL_EFFORT);
-		traialsNext.erase(begin(traialsNext),
-						  begin(traialsNext)+std::min(traialsNext.size(),LVL_EFFORT));
-		std::move(begin(traialsNext), end(traialsNext), std::back_inserter(traials_));
-		traialsNext.clear();
-		// ... run Fixed Effort until the next level 'l+1' ...
-		do {
-			Traial& traial(traialsNow.back());
-			traialsNow.pop_back();
-			assert(traial.level < LVL_MAX);
-			reachCount_[traial.level]++;
-			network.simulation_step(traial, property, *this, event_watcher);
-			if (traial.level > l) {
-				numSuccesses++;
-				traialsNext.push_back(traial);
-			} else {
-				if (property.is_rare(traial.state))
-					numSuccesses++;
-				traials_.push_back(traial);
-			}
-		} while (!traialsNow.empty());
-		// ... and estimate the probability of reaching 'l+1' from 'l'
-		Pup[l] = static_cast<double>(numSuccesses) / LVL_EFFORT;
-	}
+//	// For each threshold level 'l' ...
+//	ImportanceValue l;
+//	size_t numSuccesses(1ul);
+//	for (l = LVL_INI ; l < LVL_MAX && 0ul < traialsNext.size() ; l++) {
+//		// ... prepare the Traials to run the simulations ...
+//		numSuccesses = 0ul;
+//		const auto LVL_EFFORT(lvl_effort(impFun_->effort_of(l)));
+//		assert(0 < LVL_EFFORT);
+//		assert(traialsNow.empty());
+//		assert(!traialsNext.empty());
+//		for (auto j = 0ul ; j < LVL_EFFORT ; j++) {
+//			const bool useFresh(j >= traialsNext.size());
+//			Traial& traial(useFresh ? traials_.back() : traialsNext[j]);
+//			if (useFresh) {
+//				traials_.pop_back();
+//				traial = traialsNext[j%traialsNext.size()].get();  // copy *contents*
+//			}
+//			assert(traial.level == l);
+//			traial.depth = 0;
+//			traialsNow.push_back(traial);
+//		}
+//		assert(traialsNow.size() == LVL_EFFORT);
+//		traialsNext.erase(begin(traialsNext),
+//						  begin(traialsNext)+std::min(traialsNext.size(),LVL_EFFORT));
+//		std::move(begin(traialsNext), end(traialsNext), std::back_inserter(traials_));
+//		traialsNext.clear();
+//		// ... run Fixed Effort until the next level 'l+1' ...
+//		do {
+//			Traial& traial(traialsNow.back());
+//			traialsNow.pop_back();
+//			assert(traial.level < LVL_MAX);
+//			reachCount_[traial.level]++;
+//			network.simulation_step(traial, property, *this, event_watcher);
+//			if (traial.level > l) {
+//				numSuccesses++;
+//				traialsNext.push_back(traial);
+//			} else {
+//				if (property.is_rare(traial.state))
+//					numSuccesses++;
+//				traials_.push_back(traial);
+//			}
+//		} while (!traialsNow.empty());
+//		// ... and estimate the probability of reaching 'l+1' from 'l'
+//		Pup[l] = static_cast<double>(numSuccesses) / LVL_EFFORT;
+//	}
 }
 
 } // namespace fig  // // // // // // // // // // // // // // // // // // // //
