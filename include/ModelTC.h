raulmonti/FIG

commit 5023f41ab78d945b1edb56fb71ed9d446ffe89b0

Virtual dtors to avoid Clang compile warnings in DEBUG mode

include/ModuleScope.h
@@ -21,6 +21,9 @@ using transition_map = unordered_multimap<string, shared_ptr<TransitionAST>>;
  */
 class ModuleScope {
 public:
+	inline virtual ~ModuleScope() { clear(); }
+
+public:
     /// Static map that store the symbol table for each module
     /// indexed by the module's name
     static shared_map<string, ModuleScope> scopes;
@@ -183,7 +186,7 @@ public:
         return instance_;
 	}
 
-	~CompositeModuleScope() { clear(); }
+	virtual ~CompositeModuleScope() { clear(); }
 
 	/// Delete all information about model/modules scopes,
 	/// <b>including the one that resides in static class members</b>

build_project.sh
@@ -102,7 +102,7 @@ else
 fi
 
 # Build in release mode? Or in debug mode?
-RELEASE_BUILD=true;
+RELEASE_BUILD=false;
 
 # Cmake build options, see CMakeLists.txt
 #OPTS="$OPTS -DBUILTIN_RNG=ON";

include/ConfidenceInterval.h
@@ -117,6 +117,8 @@ public:  // Ctor
 					   bool dynamicPrecision = false,
 					   bool neverStop = false);
 
+	virtual ~ConfidenceInterval() {}
+
 public:  // Accessors
 
 	/// @copydoc numSamples_

include/ConfidenceIntervalMean.h
@@ -59,6 +59,9 @@ public:  // Ctor
 						   double precision,
 						   bool dynamicPrecision = false,
 						   bool neverStop = false);
+
+	virtual ~ConfidenceIntervalMean() {}
+
 public:  // Modifyers
 
 	/**

include/ConfidenceIntervalProportion.h
@@ -69,6 +69,8 @@ public:  // Ctor
 								 bool dynamicPrecision = false,
 								 bool neverStop = false);
 
+	virtual ~ConfidenceIntervalProportion() {}
+
 public:  // Modifyers
 
 	/**

include/ConfidenceIntervalRate.h
@@ -60,6 +60,8 @@ public:
 		ConfidenceInterval("rate", confidence, precision, dynamicPrecision, neverStop),
 		ConfidenceIntervalMean(confidence, precision, dynamicPrecision, neverStop)
 		{ /* Not much to do around here... */ }
+
+	virtual ~ConfidenceIntervalRate() {}
 };
 
 }

include/ConfidenceIntervalResult.h
@@ -42,6 +42,8 @@ namespace fig
 /// Observer class for ConfidenceInterval,
 /// used e.g. to show resulting estimates to the user.
 /// @warning Immutable: instances of this class are unfit for estimations
+/// @todo FIXME: why do we have a shared_ptr of the base class ???
+///              What on earth was I trying to do here ???
 class ConfidenceIntervalResult : public ConfidenceInterval
 {
 	/// (Derived) Instance of ConfidenceInterval we're wrapping
@@ -55,6 +57,9 @@ public:  // Ctors from the other derived classes of ConfidenceInterval
 	    ConfidenceInterval(*ci),
 	    instance_(ci)
 	{ /* Not much to do around here... */ }
+
+	inline virtual ~ConfidenceIntervalResult()
+		{ instance_.reset(); }
 
 public:  // Methods linked to the real functions of our creation class
 

include/ConfidenceIntervalWilson.h
@@ -67,6 +67,8 @@ public:  // Ctor
 							 bool dynamicPrecision = false,
 							 bool neverStop = false);
 
+	virtual ~ConfidenceIntervalWilson() {}
+
 public:  // Modifyers
 
 	/// @copydoc ConfidenceIntervalProportion::update(const double&)

commit 2f8ab159ae3b8d93623d71a690c52ad2d9866a4e

Bugfix for issue #14.
The bug was introduced in commit 5023f41 in the virtual dtor of ModelTC.
Method "clear()" must *not be called* when deleting a ModelTC object,
because the internal vectors contain shared data that is used
during later steps in model building.

include/ModelTC.h
@@ -101,7 +101,7 @@ public:
 
     ModelTC(const ModelTC& instance) = default;
 
-    virtual ~ModelTC();
+	virtual ~ModelTC() {}
 
     /// Visitor functions
     void visit(shared_ptr<Model> node) override;

include/ModuleScope.h
@@ -21,7 +21,9 @@ using transition_map = unordered_multimap<string, shared_ptr<TransitionAST>>;
  */
 class ModuleScope {
 public:
-	inline virtual ~ModuleScope() { clear(); }
+	inline virtual ~ModuleScope() {
+		// clear();  // don't clear! Later module building relies on shared data
+	}
 
 public:
     /// Static map that store the symbol table for each module

src/ModelTC.cpp
@@ -492,32 +492,32 @@ void ModelTC::check_ranged_all(shared_ptr<ModuleScope> scope) {
 }
 
 void ModelTC::visit(shared_ptr<Model> model) {
+	assert(nullptr != model);
 	//clear any previous data
 	scopes.clear();
 	globals.clear();
-    //check globals
+	//check globals
     for (auto decl : model->get_globals()) {
         //no module selected yet
-        current_scope = nullptr;
+		current_scope.reset();
         accept_cond(decl);
     }
     //check modules
-    auto& modules = model->get_modules();
-    unsigned int i = 0;
-    while (i < modules.size()) {
-        const shared_ptr<ModuleScope>& new_scope = make_shared<ModuleScope>();
-        const string &id = modules[i]->get_name();
-        if (scopes.find(id) != scopes.end()) {
+	const auto& modules(model->get_modules());
+	for (auto i = 0u ; i < modules.size() ; i++) {
+		auto new_scope(make_shared<ModuleScope>());
+		const string& id(modules[i]->get_name());
+		assert(0ul < id.length());
+		if (scopes.find(id) != scopes.end()) {
             shared_ptr<ModelAST> prev = scopes[id]->module_ast();
             put_error(TC_ID_REDEFINED(current_scope, prev, id));
         }
         new_scope->set_module_ast(modules[i]);
-        new_scope->set_module_name(modules[i]->get_name());
+		new_scope->set_module_name(id);
         //set current scope before accepting module body
         current_scope = new_scope;
         scopes[id] = new_scope;
         accept_cond(new_scope->module_ast());
-        i++;
     }
     current_scope = nullptr;
     for (auto prop : model->get_props()) {
@@ -969,7 +969,4 @@ void ModelTC::visit(shared_ptr<RateProp> prop) {
     check_type(Type::tbool, TC_WRONG_PROPERTY_EXP(prop, last_type));
     check_dnf(prop->get_type(), prop->get_expression());
     checking_property = false;
-}
-
-ModelTC::~ModelTC() {
 }